9114a8ec604b3e9767011fe5ad17ca44d2803ab0
Mohamed Elyes Ben Zina <benzina.mohamedelyes@gmail.com>
2025-04-12 16:19:13 +0200

    Refactoring

--- StringParserKata/Program.cs
+++ StringParserKata/Program.cs
@@ -23,26 +23,13 @@
                         var stringToSearch = Console.ReadLine();
                         if (!string.IsNullOrEmpty(stringToSearch))
                         {
-                            for (int i = 0; i < input.Length; i++)
+                            var i = ParseString(input, stringToSearch);
+                            if (i != -1)
                             {
-                                if (input[i] == stringToSearch[0] && i + stringToSearch.Length < input.Length)
-                                {
-                                    var found = true;
-                                    for (int j = 1; j < stringToSearch.Length; j++)
-                                    {
-                                        if (input[i + j] != stringToSearch[j])
-                                        {
-                                            found = false;
-                                            break;
-                                        }
-                                    }
-                                    if (found)
-                                    {
-                                        Console.WriteLine($"Found '{stringToSearch}' at index {i}");
-                                        break;
-                                    }
-                                }
+                                Console.WriteLine($"Found '{stringToSearch}' at index {i}");
+                                break;
                             }
+
                             Console.WriteLine($"Not Found '{stringToSearch}'");
                         }
                         else
@@ -56,6 +43,29 @@
                     }
                 }
             }
+        }
+        private static int ParseString(string input, string stringToSearch)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == stringToSearch[0] && i + stringToSearch.Length <= input.Length)
+                {
+                    var found = true;
+                    for (int j = 1; j < stringToSearch.Length; j++)
+                    {
+                        if (input[i + j] != stringToSearch[j])
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
